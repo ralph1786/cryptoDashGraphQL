e9f6eeb31433d618901e4d98e9dcdfa3373f4c40
ralph1786 <ralph1786@gmail.com>
2019-07-10 16:18:53 -0400

    added currency schema

--- schema.js
+++ schema.js
@@ -1,7 +1,34 @@
 const {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLInt,
   GraphQLList,
   GraphQLString
 } = require("graphql");
+const { firstApiKey, secondApiKey } = require("./constant");
+const axios = require("axios");
+
+const CurrencyType = new GraphQLObjectType({
+  name: "cryptocurrency",
+  fields: () => ({
+    currency: { type: GraphQLString },
+    price: { type: GraphQLString }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "rootquery",
+  fields: {
+    currencies: {
+      type: new GraphQLList(CurrencyType),
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.nomics.com/v1/currencies/ticker?key=${firstApiKey}`)
+          .then(res => res.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
